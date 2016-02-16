1ef0460c85b4504d9cd9518cdf206e76c0748ade
emensch <eli.mensch@gmail.com>
2016-02-16 17:35:31 -0500

    Set up basic route structure

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -1,10 +1,15 @@
 import React                    from 'react';
 import { Route, IndexRoute }    from 'react-router';
 
-import App                      from './components/index'
+import App                      from './components/index';
+import Home                     from './components/Home';
+import Artist                   from './components/Artist';
+import Contact                  from './components/Contact';
 
 export default (
     <Route name='app' path='/' component={App}>
-        <IndexRoute />
+        <IndexRoute component={Home} />
+        <Route path='artist/:slug' component={Artist} />
+        <Route path='contact' component={Contact} />
     </Route>
 );
