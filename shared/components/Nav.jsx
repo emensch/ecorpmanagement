1a9e4e6281b558cd05994562550727fe2e18fa98
emensch <eli.mensch@gmail.com>
2016-02-16 20:00:12 -0500

    Basic nav styling

--- shared/components/Nav.jsx
+++ shared/components/Nav.jsx
@@ -1,6 +1,6 @@
-import React                from 'react';
-import { Link, IndexLink }  from 'react-router';
-import classNames           from 'classnames';
+import React        from 'react';
+import { Link }     from 'react-router';
+import classNames   from 'classnames';
 
 export default class Nav extends React.Component {
 
@@ -10,9 +10,9 @@ export default class Nav extends React.Component {
         return (
             <div className={classes}>
                 <ul>
-                    <li><IndexLink to='/'> Home </IndexLink></li>
                     <li><Link to='/artist/argenil'> Argenil </Link></li>
                     <li><Link to='/artist/michaelblume'> Michael Blume </Link></li>
+                    <li className='separator'> | </li>
                     <li><Link to='/contact'> Contact </Link></li>
                 </ul>
             </div>
