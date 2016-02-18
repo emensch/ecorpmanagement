aaf770bbf267821acd45afb70b81016446d80a03
emensch <eli.mensch@gmail.com>
2016-02-18 04:00:22 -0500

    Additional styling for contact page

--- shared/components/Contact.jsx
+++ shared/components/Contact.jsx
@@ -9,17 +9,14 @@ export default class Contact extends React.Component {
     render() {
         let classes = classNames('contact');
         let containerClasses = classNames('contact-container');
-        let container2Classes = classNames('contact-container-inner');
         let headerClasses = classNames('contact-header');
 
         return (
             <div className={classes}>
                 <HomeButton />
                 <div className={containerClasses}>
-                    <div className={container2Classes}>
-                        <div className={headerClasses}> CONTACT </div>
-                        <ContactForm />
-                    </div>
+                    <div className={headerClasses}> CONTACT </div>
+                    <ContactForm />
                 </div>
             </div>
         );
