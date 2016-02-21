a0755ced312ec17977f8758950365ed7a90c8648
emensch <eli.mensch@gmail.com>
2016-02-20 19:03:06 -0500

    Create config file

---
+++ example_env.config.js
@@ -0,0 +1,3 @@
+// Set these to your values and rename file to env.config.js
+process.env['PG_USERNAME'] = 'username';
+process.env['PG_PASSWORD'] = 'password';
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 require('babel-core/register')({});
 require('babel-polyfill');
 
+var config = require('../env.config.js');
 var server = require('./server').default;
 
 const PORT = process.env.PORT || 1337;
