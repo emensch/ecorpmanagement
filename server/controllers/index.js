9151ad113c17d583ba9a2c8f3be7cfa1ad51c086
emensch <eli.mensch@gmail.com>
2016-02-20 19:32:10 -0500

    Install & use body-parser

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,11 @@
-import express  from 'express';
-import artists  from './artists';
-import users    from './users';
+import express      from 'express';
+import bodyParser   from 'body-parser'
+import artists      from './artists';
+import users        from './users';
 
 const router = express.Router();
+
+router.use(bodyParser.json());
 
 router.use('/artists', artists);
 router.use('/users', users);
