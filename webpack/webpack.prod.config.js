e20b6bb63945eb8ae314560479e35a8bf0c45d6c
emensch <eli.mensch@gmail.com>
2016-05-14 00:35:21 -0400

    Add occurrence order & define plugin to webpack prod config

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -40,6 +40,12 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
         new ExtractTextPlugin('style.css', {
             allChunks: true
         })
